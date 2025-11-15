36bece0081e2f74a3b444f615cf3ad0dad6a56af
Partha Mondal <parthamondal03620@gmail.com>
2025-11-15 18:50:37 +0530

    Fixed deploy issue

--- server/src/routes/community.routes.js
+++ server/src/routes/community.routes.js
@@ -3,5 +3,4 @@ import { createCommunity } from "../controllers/Community.controller.js";
 const router = Router();
 // router.get("/", getCommunities);
 router.post("/create", createCommunity);
-
 export default router;
