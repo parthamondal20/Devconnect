27d2d08ba9bdafcfd4dfeaf7f680ed019eaef059
Parthamondal <parthamondal03620@gmail.com>
2025-12-03 15:32:54 +0530

    extends api call timeout

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
-  timeout: 10000,
+  timeout: 50000,
 });
 
 let isRefreshing = false;
